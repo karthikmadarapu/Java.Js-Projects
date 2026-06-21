6556d15f89f606d12d277f3817f4d668ff3bb148
Karthik Madarapu <karthikmadarapu21@gmail.com>
2026-06-21 11:42:48 -0400

    simple addition calculator

--- Js_projects/JS_FUND/practice_1.js
+++ Js_projects/JS_FUND/practice_1.js
@@ -7,8 +7,24 @@ textInput.addEventListener("input", function(){
     displayCounter.textContent = "CHARACTERS: " + textcount; 
 })
 
+// Small Addition calculator 
+
+const numberOne = document.getElementById("input01");
+const numberTwo = document.getElementById("input02");
+const button = document.getElementById("myButton");
+const totalOutput = document.getElementById("total");
+
+button.addEventListener("click",function(){
+    const val1 = Number(numberOne.value);
+    const val2 = Number(numberTwo.value);
+
+    const sum =  val1 + val2 ;
+
+    totalOutput.textContent = "Result :" + sum ;
+
+});
 
 
 
 
- 
+
