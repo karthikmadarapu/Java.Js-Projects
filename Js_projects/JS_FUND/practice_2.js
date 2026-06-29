d8367d4aa4a0dc77f6694a178ec15fa4905fbea2
Karthik Madarapu <karthikmadarapu21@gmail.com>
2026-06-29 16:38:04 -0400

    password Validator

--- Js_projects/JS_FUND/practice_2.js
+++ Js_projects/JS_FUND/practice_2.js
@@ -23,3 +23,42 @@
 
 
     });
+
+
+
+    // Password Generator
+
+    const  inputPass = document.getElementById("passwordInput");
+    const lengthVerify = document.getElementById("lengthRule");
+    const verifyNum = document.getElementById("numberRule");
+
+
+    inputPass.addEventListener('input', () => {
+        const password = inputPass.value;
+      
+         if(password.length >= 8){
+           
+             lengthVerify.textContent = "✔ At least 8 characters";
+             lengthVerify.style.color = "green";
+
+         }
+         else{
+            lengthVerify.textContent = "✖ At least 8 characters";
+             lengthVerify.style.color = "red";
+         }
+
+         const hasNum = /\d/.test(password);
+
+         if(hasNum){
+             verifyNum.textContent = "✔ Contains at least one number";
+             verifyNum.style.color = "green"
+         }
+         else{
+            
+             verifyNum.textContent = "✖ Contains at least one number";
+             verifyNum.style.color = "red"
+         }
+
+
+    });
+   
