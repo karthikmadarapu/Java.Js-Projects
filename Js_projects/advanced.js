18bc500b38993348fbe81fd7aaf3da5ccf6f3ed2
Karthik Madarapu <karthikmadarapu21@gmail.com>
2026-07-06 06:34:36 -0400

    bank account / object closure exmaple about thier scopes

---
+++ Js_projects/advanced.js
@@ -0,0 +1,24 @@
+const createBankAcc = (initialAmount) =>{
+    let  balance = initialAmount ;
+
+    return {
+        deposit(amount){
+            balance += amount;
+            return `Balance: ${balance}`;
+
+        },
+        withdraw(amount){
+         if(amount > balance) return `Insufficient funds, try again!`;
+         balance -= amount;
+         return `Balance: ${balance}`;
+        } 
+    };
+
+
+
+};
+
+
+const  myAcc = createBankAcc(2000);
+console.log(myAcc.deposit(500));
+console.log(myAcc.balance);
