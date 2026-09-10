2c09a49c8d2ef8a20723fd43f129e0da414c3b31
Karthik Madarapu <karthikmadarapu21@gmail.com>
2026-09-10 19:51:15 -0400

    small conditional game btween constructors and conditionals

--- java_only/src/Main.java
+++ java_only/src/Main.java
@@ -11,6 +11,19 @@ public class Main {
         System.out.println(students_101.name);
         System.out.println(students_102.name);
 
+        for(int i= 0; i < 2; i++){
+
+            if(students_101.age < 30){
+                System.out.println(students_101.name);
+            }
+            else if(students_102.age < 20){
+                System.out.println(students_102.name);
+            }
+            else{
+                System.out.println("doesnt exist !!!");
+            }
+        }
+
 
 
 
