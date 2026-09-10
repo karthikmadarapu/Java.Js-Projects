36e940450fad5f910452881d9e006a142247bb61
Karthik Madarapu <karthikmadarapu21@gmail.com>
2026-09-10 19:42:44 -0400

    getting started with java

---
+++ java_only/src/Main.java
@@ -0,0 +1,18 @@
+//TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
+// click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
+public class Main {
+    public static void main(String[] args) {
+
+
+        Students students_101 = new Students(35, "Garry", true );
+        Students students_102 = new Students(28, "manny", false );
+
+
+        System.out.println(students_101.name);
+        System.out.println(students_102.name);
+
+
+
+
+    }
+}
---
+++ java_only/src/Students.java
@@ -0,0 +1,13 @@
+public class Students {
+
+    int age;
+    String name;
+    boolean isStudent;
+
+    public Students( int age,String name , boolean isStudent){
+        this.age = age;
+        this.name = name;
+        this.isStudent = isStudent;
+
+    }
+}
